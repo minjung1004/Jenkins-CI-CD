9ba0f7be2e82972477148e0aaf2226ec3a0cdbcd
Min <minjung10402@gmail.com>
2025-11-12 17:09:07 -0800

    add space

--- simple_code.py
+++ simple_code.py
@@ -17,8 +17,8 @@ def power(a, b):
     return a ** b
 
 if __name__ == "__main__":
-    print("Addition of 5 and 7 is: ", add(5, 7))
-    print("Subctraction of 5 from 10 is: ", subtract(10, 5))
-    print("Multiplication of 7 and 8 is: ", multiply(7, 8))
-    print("Divison of 100 by 25 is: ", divide(100, 25))
-    print("2 to the power of 7 is: ", power(2, 7))
+    print("Addition of 5 and 7 is:", add(5, 7))
+    print("Subtraction of 5 from 10 is:", subtract(10, 5))
+    print("Multiplication of 7 and 8 is:", multiply(7, 8))
+    print("Division of 100 by 25 is:", divide(100, 25))
+    print("2 to the power of 7 is:", power(2, 7))
